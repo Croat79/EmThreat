ba8328667183c951678df8e1a2c8b8daa801fc0d
Zach <18583032+steelsleuth@users.noreply.github.com>
2021-07-28 16:33:43 -0500

    Update chunking.py
    Removed a line in block_gen that appended an empty array to our blocks. These blocks should have already been generated. If the 2D array generation is wrong, we can fix that outside of the loop.

--- source/chunking.py
+++ source/chunking.py
@@ -1,23 +1,31 @@
 '''
 Chunking will do the following
+- data should be sorted before chunking to allow better representation
 - creating blocks of size 1000
 - each block will have LCS run on it
-- the top 20 results from each block are saved to a new list
+- the top 100 results from each block are saved to a new list
 - finds the top 100 most occuring results
 '''
 import math
 
+# Returns an array of X blocks.
+# If 1000 results are passed to 10 blocks, then each block has 100 results. 
+# [[block1], [block2],...[blockn]]
+# [[url11, url12, url1n],...[urln1, urln2, urlnn]]
 def block_gen(results):
     block_length = 10 # 10 for testing
-    # Calculate the total blocks.
+    # Calculate the total blocks used.
     block_total = math.ceil(len(results) / block_length)
+    # Create a 2D array to store blocks.
+    # [[block1], [block2],...[blockn]]
     blocks = [] * block_total
     # Go over each new block.
     for i in range(0, len(results), block_length):
-        blocks.append([])
-        # Add values to each block.
+        # Iterate over the range of i to the block length. 
         for j in range(i, i + block_length ):
+            # Go to the ith block and append the jth URL from results.
             blocks[i//block_total].append(results[j])
+    # Test output of blocks
     return blocks
 
 def block_lcs(blocks):
