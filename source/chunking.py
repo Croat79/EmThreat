47296c274f8090ee21145f8c50f1ad4ae71b0bbc
Zach <18583032+steelsleuth@users.noreply.github.com>
2021-07-28 20:07:35 -0500

    Update chunking.py

--- source/chunking.py
+++ source/chunking.py
@@ -6,13 +6,13 @@ The top results from each block are saved to a new list.
 That final list is ran through LCS one last time if desired.
 '''
 import math
-
+BLOCK_SIZE = 1000
 # Returns an array of X blocks.
 # If 1000 results are passed to 10 blocks, then each block has 100 results. 
 # [[block1], [block2],...[blockn]]
 # [[url11, url12, url1n],...[urln1, urln2, urlnn]]
 def block_gen(results):
-    block_length = 10 # 10 for testing
+    block_length = BLOCK_SIZE # 10 for testing
     # Calculate the total blocks used.
     block_total = math.ceil(len(results) / block_length)
     # Create a 2D array to store blocks.
